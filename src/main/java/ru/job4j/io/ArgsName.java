8d10e1f01dad4ec486dddbda107e50041dc08348
evfedorovyh <jhon2002@mail.ru>
2025-03-12 01:52:55 +0300

    Тестовое задание. Утилита "поиск файлов". [#783]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -44,12 +44,4 @@ public class ArgsName {
         names.parse(args);
         return names;
     }
-
-    public static void main(String[] args) {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
-        System.out.println(jvm.get("Xmx"));
-
-        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
-        System.out.println(zip.get("out"));
-    }
 }
